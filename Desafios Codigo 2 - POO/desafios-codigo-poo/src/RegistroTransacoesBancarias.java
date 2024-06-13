a2a6534da318925ef775d255c47b26af1a972fd3
jessieFerrS <114115950+jessieFerrS@users.noreply.github.com>
2024-06-13 09:08:42 -0300

    Commit Desafios de Codigo Poo

---
+++ Desafios Codigo 2 - POO/desafios-codigo-poo/src/RegistroTransacoesBancarias.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RegistroTransacoesBancarias {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Lê a Entrada que informa o saldo inicial da conta
+        double saldo = scanner.nextDouble();
+
+        // Lê a Entrada com a quantidade total de transações
+        int quantidadeTransacoes = scanner.nextInt();
+
+        // Lista para armazenar as transações
+        List<String> transacoes = new ArrayList<>();
+
+        // Loop para iterar sobre as transações
+        char tipoTransacao = 0;
+        double valorTransacao = 0;
+
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+            // Lê a Entrada com o tipo de transação (D para depósito ou S para saque)
+            // O método "toUpperCase" padroniza o tipo de transação com a letra maiúscula
+            tipoTransacao = scanner.next().toUpperCase().charAt(0);
+            // Lê a Entrada com o valor da transação
+            valorTransacao = scanner.nextDouble();
+
+            // Atualiza o saldo da conta e adicionar a transação à lista
+            if (tipoTransacao == 'D') {
+                saldo += valorTransacao;
+                transacoes.add("Deposito de " + valorTransacao);
+            } else if (tipoTransacao == 'S') {
+                saldo -= valorTransacao;
+                transacoes.add("Saque de " + valorTransacao);
+            } else {
+                System.out.println("Opção inválida. Utilize D para depósito ou S para saque.");
+                i--; // Decrementa o índice para repetir a iteração
+            }
+
+        }
+        System.out.printf("Saldo: " + saldo);
+        System.out.println("\nTransacoes:");
+
+        int indice = 1;
+        for(String t: transacoes){
+            System.out.println(indice + ". " + t);
+            indice++;
+        }
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
